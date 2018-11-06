b60293dd43395f82e35c0b56b905622c00b29c63
Nikolaos Papazian <nikolaspapazian@gmail.com>
2018-11-06 12:46:55 +0200

    Add project files.

---
+++ CiteApp v1.2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CiteApp_v1._2.Startup))]
+namespace CiteApp_v1._2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
